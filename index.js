199105aa4349be9b1ce611ef140637807d068a77
Chan Li Liao <cliao119@gmail.com>
2022-01-10 14:36:10 -0500

    connection cancel

--- index.js
+++ index.js
@@ -57,9 +57,4 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 // let us know the server is running
-app.listen(
-  PORT,
-  console.log(`Server running in 
-  // ${process.env.NODE_ENV} 
-  on port ${PORT}`)
-);
+app.listen(PORT, console.log(`Server running in on port ${PORT}`));
